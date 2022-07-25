5d64859e2d794b7c7b1e105ea35513350ff7b428
leosantosw <leonardosantosx0077@gmail.com>
2022-07-24 23:45:36 -0300

    feature: create initial html to page home

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.main`
+ 
+`
+export const FormContainer = styled.div`
+ 
+`
+export const CountdownContainer = styled.div`
+ 
+`
